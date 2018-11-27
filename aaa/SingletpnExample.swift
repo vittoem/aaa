65e083fe574c553c49dfcb2b7c5ef1e395b9454a
Vittorio Emanuele Carlino <vittorioemanuelecarlino@par-134.local>
2018-11-27 16:27:03 +0100

    wersfsf

---
+++ aaa/SingletpnExample.swift
@@ -0,0 +1,16 @@
+//
+//  SingletpnExample.swift
+//  aaa
+//
+//  Created by Vittorio Emanuele Carlino on 27/11/2018.
+//  Copyright © 2018 Vittorio Emanuele Carlino. All rights reserved.
+//
+
+import Foundation
+
+class SingletonExample {
+    static let sharedInstance = SingletonExample()
+    
+    private init() {}
+    
+}
