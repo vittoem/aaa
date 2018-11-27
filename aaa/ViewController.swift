356b14ec4df37920f3fd52ff0e196ce35d692917
Vittorio Emanuele Carlino <vittorioemanuelecarlino@par-134.local>
2018-11-27 16:10:08 +0100

    Initial Commit

---
+++ aaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aaa
+//
+//  Created by Vittorio Emanuele Carlino on 27/11/2018.
+//  Copyright © 2018 Vittorio Emanuele Carlino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
